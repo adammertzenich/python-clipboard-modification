aa0be26e9e42195beeb96e658fb7239c79a04a08
adammertzenich <adam.mertzenich@decorahvikings.k12.ia.us>
2017-05-19 09:49:05 -0500

    Update Clipboard MAC.py

--- Clipboard MAC.py
+++ Clipboard MAC.py
@@ -1,6 +1,7 @@
 from AppKit import NSPasteboard, NSArray
+# Requires https://pythonhosted.org/pyobjc/
 
 pb = NSPasteboard.generalPasteboard()
 pb.clearContents()
-a = NSArray.arrayWithObject_("7")
+a = NSArray.arrayWithObject_("This is the string that will be copied to your clipboard.")
 pb.writeObjects_(a)
